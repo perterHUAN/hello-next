03788dc960394ff5122c29c50a10bd91e99ccc05
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-04-12 10:51:58 +0800

    feat: server timestamp

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function Home() {
   return (
-    <main>
-      <h1>Hello Next!</h1>
-    </main>
+    <>
+      <main>
+        <h1>Hello Next!</h1>
+      </main>
+      <footer>Page rendered on {new Date().toLocaleString()}</footer>
+    </>
   );
 }
 
